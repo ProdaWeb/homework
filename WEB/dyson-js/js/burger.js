cdb28c29604be86380df9416017c7cfb6ae20bc2
ProdaWeb <danil-skorpion@mail.ru>
2026-06-03 16:16:39 +0500

    Delete WEB/dyson-js directory

--- WEB/dyson-js/js/burger.js
+++
@@ -1,9 +0,0 @@
-export function burgerMenuFunc(header) {
-	const menuBtn = document.querySelector(".header__burger-btn");
-	const body = document.body;
-
-	menuBtn.addEventListener("click", () => {
-		header.classList.toggle("open");
-		body.classList.toggle("lock");
-	});
-}
--- WEB/dyson-js/js/header-shadow.js
+++
@@ -1,9 +0,0 @@
-export function headerShadowFunc(header) {
-	window.addEventListener("scroll", () => {
-		if (window.scrollY > 0) {
-			header.classList.add("shadow");
-		} else {
-			header.classList.remove("shadow");
-		}
-	});
-}
